6ba701f338383df98f36537bc3c35389a8c13d3a
huyangh <huyangh123@gmail.com>
2018-08-09 09:53:04 +0800

    first test OK

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,122 @@
-//index.js
-//获取应用实例
-var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
-var qqmapsdk;
+
+var util = require('../../utils/util.js')
+
 const app = getApp()
 
 Page({
   data: {
-    udata: 'Hello World',
-    heartrate:70,
-    spo2:98,
-    location:"120E,30N"
+    heartrate:'',
+    spo2:'',
+    lo_gps:'',
+    lo_lbs:'',
+    la_gps:'',
+    la_lbs:'',
+    batlvl:'',
+    signal:'',
+//    sn:'001221A00B80'
   },
-  //事件处理函数
-  // bindViewTap: function() {
-  //   wx.navigateTo({
-  //     url: '../logs/logs'
-  //   })
-  // },
+  
+  bindViewTap: function() {
+    // if(lo_gps != 0 && la_gps !=0){
+    //   wx.openLocation({
+    //     longitude: lo_gps,
+    //     latitude: la_gps
+    //   })
+    // }else{
+    //   wx.openLocation({
+    //     longitude: lo_lbs,
+    //     latitude: la_lbs
+    //   })
+    // }
+    wx.openLocation({
+      latitude: '',
+      longitude: '',
+    })
+  },
+
   onLoad: function () {
+    
   },
 
   searchSN:function(){
-    console.log("button touch");
-    
+    console.log("button tap");
+    var that = this;
+    // if(this.data.sn.length == 12){
+    //   wx.showToast({
+    //     title: '查询成功',
+    //     icon: 'success',
+    //     duration: 2000
+    //   })
+    // }else{
+    //   wx.showToast({
+    //     title: '序列号格式错误',
+    //     icon: 'fail',
+    //     duration: 2000
+    //   })
+    // }
+    wx.request({
+      url: 'https://api.hizyf.com/DM-open-service/auth/getToken',
+      data: {
+        corporateId: "3fb0139916be4d53a73dbdf0ae452571",
+        corporatePasswd: "zxcvbnm123"
+      },
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/json',
+      },
+      success: function (res) {
+        console.log(res.data);
+        that.getdata(res.data.token);
+      }
+    })
+  },
+
+  // sninput:function(e){
+  //   this.setData({
+  //     sn:e.detail.value
+  //   })
+  // },
+
+  getdata: function (token) {
+    // 获取数据
+    var that = this;
+    wx.request({
+      method:'POST',
+      url:    'https://api.hizyf.com/DM-open-service/serviceSum/deviceDataNew/3fb0139916be4d53a73dbdf0ae452571',
+      data: {
+        'deviceSn': '001221A00B80',
+        'fieldId': ''
+      },
+      header: {
+        'content-type': 'application/json',
+        'token': token
+      },
+      success: function (res) {
+        console.log(res.data.t),
+        that.setData({
+          signal: res.data.t.items[1].numValues,
+          batlvl: res.data.t.items[2].numValues,
+          lo_gps: res.data.t.items[5].numValues,
+          la_gps: res.data.t.items[6].numValues,
+          lo_lbs: res.data.t.items[7].numValues,
+          la_lbs: res.data.t.items[8].numValues,
+          heartrate: res.data.t.items[9].numValues,
+          spo2: res.data.t.items[10].numValues,
+
+
+         })
+      },
+      fail:function (){
+        wx.showToast({
+          title: '请求数据失败',
+          icon:'fail',
+          duration:2000
+        })
+      }
+    })
   }
+
+
 })
+
+
---
+++ pages/map/map.js
@@ -0,0 +1,75 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // markers: [{
+    //   iconPath: "../../images/red1.png",
+    //   id: 0,
+    //   latitude: 40.002607,
+    //   longitude: 116.487847,
+    //   width: 30,
+    //   height: 45
+    // }]
+  },
+  
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+     
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
