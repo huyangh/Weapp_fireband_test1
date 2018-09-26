6c387f3bcebc5cd21f32eb56b227ea8b700039d3
huyangh <huyangh123@gmail.com>
2018-09-26 10:19:46 +0800

    almost OK

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,75 +7,84 @@ Page({
   data: {
     heartrate:'',
     spo2:'',
-    lo_gps:'',
-    lo_lbs:'',
-    la_gps:'',
-    la_lbs:'',
+    lo_gps:0,
+    lo_lbs:0,
+    la_gps:0,
+    la_lbs:0,
     batlvl:'',
     signal:'',
-//    sn:'001221A00B80'
+    SN:'',
+    status:'',
+    collectTime1:'',
+    collectTime2: '',
+
   },
   
   bindViewTap: function() {
-    // if(lo_gps != 0 && la_gps !=0){
-    //   wx.openLocation({
-    //     longitude: lo_gps,
-    //     latitude: la_gps
-    //   })
-    // }else{
-    //   wx.openLocation({
-    //     longitude: lo_lbs,
-    //     latitude: la_lbs
-    //   })
-    // }
-    wx.openLocation({
-      latitude: '',
-      longitude: '',
-    })
+    if(this.data.lo_gps != 0 && this.data.la_gps !=0){
+      wx.openLocation({
+        longitude: this.data.lo_gps,
+        latitude: this.data.la_gps
+      })
+    }else{
+      wx.openLocation({
+        longitude: this.data.lo_lbs,
+        latitude: this.data.la_lbs
+      })
+    }
+    
   },
 
   onLoad: function () {
     
   },
 
-  searchSN:function(){
-    console.log("button tap");
-    var that = this;
-    // if(this.data.sn.length == 12){
-    //   wx.showToast({
-    //     title: '查询成功',
-    //     icon: 'success',
-    //     duration: 2000
-    //   })
-    // }else{
-    //   wx.showToast({
-    //     title: '序列号格式错误',
-    //     icon: 'fail',
-    //     duration: 2000
-    //   })
-    // }
-    wx.request({
-      url: 'https://api.hizyf.com/DM-open-service/auth/getToken',
-      data: {
-        corporateId: "3fb0139916be4d53a73dbdf0ae452571",
-        corporatePasswd: "zxcvbnm123"
-      },
-      method: 'POST',
-      header: {
-        'Content-Type': 'application/json',
-      },
-      success: function (res) {
-        console.log(res.data);
-        that.getdata(res.data.token);
-      }
-    })
+
+  sninput:function(e){
+    var snlist = new Array(100);
+    snlist[0] = '001221A00B80';
+    
+    if(e.detail.value <= 100 && e.detail.value >= 1){
+      this.setData({
+        SN: snlist[e.detail.value - 1]
+      })
+    }else{
+      
+    }
+    
+    
   },
 
-  // sninput:function(e){
-  //   this.setData({
-  //     sn:e.detail.value
-  //   })
-  // },
+  searchSN:function(){
+    console.log("button tap");
+    if(this.data.SN != null){
+      console.log("SN:" + this.data.SN);
+      var that = this;  
+
+      wx.request({
+        url: 'https://api.hizyf.com/DM-open-service/auth/getToken',
+        data: {
+          corporateId: "3fb0139916be4d53a73dbdf0ae452571",
+          corporatePasswd: "zxcvbnm123"
+        },
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json',
+        },
+        success: function (res) {
+          console.log(res.data);
+          that.getdata(res.data.token);
+        }
+      })
+    }else{
+      wx.showToast({
+        title: '请重新输入正确的设备编号',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+  },
+
 
   getdata: function (token) {
     // 获取数据
@@ -84,7 +93,7 @@ Page({
       method:'POST',
       url:    'https://api.hizyf.com/DM-open-service/serviceSum/deviceDataNew/3fb0139916be4d53a73dbdf0ae452571',
       data: {
-        'deviceSn': '001221A00B80',
+        'deviceSn': this.data.SN,
         'fieldId': ''
       },
       header: {
@@ -92,20 +101,46 @@ Page({
         'token': token
       },
       success: function (res) {
-        console.log(res.data.t),
-        that.setData({
-          signal: res.data.t.items[1].numValues,
-          batlvl: res.data.t.items[2].numValues,
-          lo_gps: res.data.t.items[5].numValues,
-          la_gps: res.data.t.items[6].numValues,
-          lo_lbs: res.data.t.items[7].numValues,
-          la_lbs: res.data.t.items[8].numValues,
-          heartrate: res.data.t.items[9].numValues,
-          spo2: res.data.t.items[10].numValues,
+      
+        console.log(res.data.t)
 
-
-         })
+        // var batbuf = res.data.t.items[2].numValues;
+        // var logpsbuf = res.data.t.items[5].numValues;
+        // var lagpsbuf = res.data.t.items[6].numValues;
+        // var lolbsbuf = res.data.t.items[7].numValues;
+        // var lalbsbuf = res.data.t.items[8].numValues;
+        // var hrbuf = res.data.t.items[10].numValues;
+        // var spo2buf = res.data.t.items[9].numValues;
+        if (res.data.t.items[10].numValues == 0){
+          that.setData({
+            
+            batlvl: res.data.t.items[2].numValues,
+            lo_gps: res.data.t.items[5].numValues,
+            la_gps: res.data.t.items[6].numValues,
+            lo_lbs: res.data.t.items[7].numValues,
+            la_lbs: res.data.t.items[8].numValues,
+            
+            collectTime1: new Date(res.data.t.collectDate).toLocaleDateString().replace(/\//g, "-") ,
+            collectTime2: new Date(res.data.t.collectDate).toTimeString().substr(0, 8),
+          
+          })
+        } else if (res.data.t.items[2].numValues == 0){
+          that.setData({
+            heartrate: res.data.t.items[10].numValues,
+            spo2: res.data.t.items[9].numValues,
+            status: res.data.t.onlineStatus,
+          })
+        }
+        
+        if (res.data.t.items[2].numValues != 0) {
+          wx.showToast({
+            title: '查询成功',
+            icon: 'success',
+            duration: 2000
+          })
+        }
       },
+
       fail:function (){
         wx.showToast({
           title: '请求数据失败',
@@ -114,8 +149,46 @@ Page({
         })
       }
     })
-  }
+  },
 
+  onShareAppMessage: function () {
+
+      return {
+
+        title: 'Fireband',
+
+        desc: '查看手环信息',
+
+        path: '/index/index'
+
+      }
+
+    },
+
+  onPullDownRefresh: function () {
+    wx.stopPullDownRefresh();
+    console.log("refresh!");
+    if (this.data.SN != null) {
+      console.log("SN:" + this.data.SN);
+      var that = this;
+
+      wx.request({
+        url: 'https://api.hizyf.com/DM-open-service/auth/getToken',
+        data: {
+          corporateId: "3fb0139916be4d53a73dbdf0ae452571",
+          corporatePasswd: "zxcvbnm123"
+        },
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json',
+        },
+        success: function (res) {
+          console.log(res.data);
+          that.getdata(res.data.token);
+        }
+      })
+    }
+  }
 
 })
 
